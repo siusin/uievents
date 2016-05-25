a6a3eb9f0c979f9f6b706203a18a345be709d1f2
siusin <siusinng@gmail.com>
2016-05-25 14:09:56 -0400

    set up auto build

---
+++ .deploy-output.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -ev
+STATUS=`git log -1 --pretty=oneline`
+
+python build.py
+cp index.html ./out/
+mkdir ./out/tests
+mkdir ./out/images
+mkdir ./out/tools
+cp tests/* ./out/tests
+cp images/* ./out/images
+cp tools/* ./out/tools
+cp style.css ./out/
+
+cd out
+git init
+git config user.name "Travis-CI"
+git config user.email "travis-ci"
+ls
+git add .
+git commit -m "Built by Travis-CI: $STATUS"
+git status
+
+GH_REPO="@github.com/siusin/uievents.git"
+FULL_REPO="https://$GH_TOKEN$GH_REPO"
+git push --force --quiet $FULL_REPO master:gh-pages > /dev/null 2>&1
